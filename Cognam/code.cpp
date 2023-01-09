2de0bb3e8fc08ab1f1f613dd8dc061227db4cc26
Ravi Prakash Singh <ssingh.raviprakash@gmail.com>
2023-01-10 04:33:40 +0530

    old files reorganised and some files removed

--- Cognam/code.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main()
-{
-    int a = 6, b = 6;
-    a = a & (a - 1);
-    cout << a << endl;
-}
--- Prakhar/new.py
+++
@@ -1,3 +0,0 @@
-print(format("Evening", "15s"), end="*")
-print(format(6969, "5d"), end="*")
-print(format(456.98752, "3,.4f"))
